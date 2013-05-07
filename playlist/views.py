2f179babd249670c9d07104e9dff4f86307cd320
Peng Xu <xupeng1@msu.edu>
2013-05-06 22:57:58 -0500

    fixed indent in views.py

--- playlist/views.py
+++ playlist/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.template import Context, loader
 
 def index(request):
-   	context = Context({})
+	context = Context({})
 	template = loader.get_template('playlist/index.html')
 	
 	return HttpResponse(template.render(context))    
