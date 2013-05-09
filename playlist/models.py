a8aac79f7b723bb19793f7cd49fd4fbd67c8ce8c
Peng Xu <xupeng1@msu.edu>
2013-05-09 00:08:27 -0500

    User can only vote once per song now

--- playlist/models.py
+++ playlist/models.py
@@ -10,6 +10,8 @@ class SongContainer:
 		self.votecount = votecount
 		# unique 160-bit id
 		self.songid = hashlib.sha256(title.encode('utf-8')).hexdigest()
+		self.upbtndisable = "enable"
+		self.downbtndisable = "enable"
 	## return a JSON representation of this object
 	def writeToJSON(self):
 		json = {}
@@ -17,6 +19,9 @@ class SongContainer:
 		json["artist"] = self.artist
 		json["votecount"] = self.votecount
 		json["songid"] = self.songid
+		# a new variable to disable the up down btn
+		json["upbtndisable"] = self.upbtndisable
+		json["downbtndisable"] = self.downbtndisable
 		return json
 
 	def updateVoteCount(delta): 
