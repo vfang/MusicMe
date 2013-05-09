ca807867d3d96ba2a245305311cc29dd36c444ed
Moritz Gellner <MoritzGellner@Moritzs-MacBook-Pro-2.local>
2013-05-09 18:43:20 -0500

    trying to fix db for heroku

--- MusicMe/settings.py
+++ MusicMe/settings.py
@@ -11,13 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'musicme.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
+        'USER': 'MoritzGellner',
         'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'HOST': '',                      # Set to empty string for default.
     }
 }
 
