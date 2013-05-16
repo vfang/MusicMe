cdeba4a5e6a2ad9aa72263ebeb7ceef0465069b0
vfang <frogg87@gmail.com>
2013-05-15 23:52:33 -0500

    Enabled functionality for changing playlist ID from URL

--- playlist/views.py
+++ playlist/views.py
@@ -1,19 +1,31 @@
 from django.http import HttpResponse
 from django.template import Context, loader
+from django.shortcuts import render, get_object_or_404
 
-from playlist.models import SongContainer
+from playlist.models import Song, Playlist
 from playlist.utils import *
 
 import json
 
 def index(request):
-	
-	# should get this from db, obviously
-	SONGS = [SongContainer("Mirrors","Justin Timberlake",10),
-			 SongContainer("Stay","Rihanna",20),
-			 SongContainer("Example 3","Moritz",21)]
+	pid = request.GET.get('playlist')
+
+	if (pid is None):
+		context = Context()
+		template = loader.get_template('playlist/index.html')
+
+		return HttpResponse(template.render(context))    		
+
+	playlist = get_object_or_404(Playlist, pk=pid)
+	SONGS = []
+
+	for song in playlist.songs.all():
+		SONGS.append(song)
+	print SONGS
+
 	currentSongList = make_song_json_list(SONGS)
+	print currentSongList
 	context = Context({'song_list': currentSongList})
 	template = loader.get_template('playlist/index.html')
 
-	return HttpResponse(template.render(context))    
+	return HttpResponse(template.render(context))
