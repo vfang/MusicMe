b58ba305c9be3bd21a419bcee83b2140b57ee032
Moritz Gellner <MoritzGellner@Moritzs-MacBook-Pro-2.local>
2013-05-28 11:47:34 -0500

    implemented cookie to store votes between refreshes, and some refactoring

---
+++ playlist/static/playlist/js/cookiejar.js
@@ -0,0 +1,51 @@
+(function(voteCookie,$,undefined){
+	voteCookie.NAME = "musicme-vote-cookie";
+	var SONGMAP = loadSongmap();
+	voteCookie.toggleSongVoteStatus = function(songid,voteDelta) { // vote is either +1 or -1
+		if (SONGMAP[songid] == undefined) {
+			SONGMAP[songid] = voteDelta;
+		} else {
+			if (SONGMAP[songid] == voteDelta) {
+				SONGMAP[songid] = undefined;
+			} else {
+				SONGMAP[songid] = voteDelta;
+			}
+		}
+		// set cookie
+		$.cookie(voteCookie.NAME,JSON.stringify(SONGMAP));
+		console.log("cookie value: ",$.cookie(voteCookie.NAME));
+	}
+
+	voteCookie.getSongmap = function() {
+		var cookieVal = $.cookie(voteCookie.NAME);
+		if (cookieVal) {
+			SONGMAP = JSON.parse(cookieVal);
+		}
+		return SONGMAP;
+	}
+	function loadSongmap() {
+		var cookieVal = $.cookie(voteCookie.NAME);
+		if (cookieVal) {
+			return JSON.parse(cookieVal);
+		}
+		return {};
+	}
+
+	voteCookie.restoreVotes = function() {
+		var songmap = voteCookie.getSongmap();
+	      for (var i in handleList.SONG_LIST) {
+	        var song = handleList.SONG_LIST[i];
+	        if (songmap[song.songid] != undefined) {
+	          var vote_val = songmap[song.songid];
+	          if (vote_val == 1) {
+	            handleList.SONG_LIST[i].upbtndisable = "True";
+	            console.log("upbtndisabled for songid = ",song.songid);
+	          } else {
+	            handleList.SONG_LIST[i].downbtndisable = "True";
+	            console.log("downbtndisabled for songid = ",song.songid);
+	          }
+	        }
+	      }
+	}
+
+}( window.voteCookie = window.voteCookie || {}, jQuery ));
---
+++ playlist/static/playlist/js/listhandler.js
@@ -0,0 +1,150 @@
+$.getScript("static/playlist/js/cookiejar.js");
+
+/* Proper coding practice: http://enterprisejquery.com/2010/10/how-good-c-habits-can-encourage-bad-javascript-habits-part-1/ */
+//Self-Executing Anonymous Func: Part 2 (Public & Private)
+(function( handleList, $, undefined){
+
+      // check if a dictionary is empty
+      handleList.isEmpty = function(dict) {
+        for (var prop in dict) {
+          if (dict.hasOwnProperty(prop)) {
+            return false;
+          }
+        }
+        return true;
+      };
+
+      // dictionary with key=songid, value=votecount
+      var CACHED_VOTES = {};
+      // refresh every minute
+      var REFRESH_INTERVAL = 10000;
+
+
+      // TODO: change votecount on backend
+      handleList.changeVote = function(delta,id) { 
+        for (var i in handleList.SONG_LIST) {
+          if (handleList.SONG_LIST[i].songid == id) {
+             var params = queryString();
+
+            if (delta > 0) {
+              if (handleList.SONG_LIST[i].upbtndisable == "False"){
+                if(handleList.SONG_LIST[i].downbtndisable == "True"){
+                  handleList.SONG_LIST[i].votecount += delta*2;
+                  delta = 2;
+                  handleList.SONG_LIST[i].upbtndisable = "True";
+                  handleList.SONG_LIST[i].downbtndisable = "False";
+                }
+                else{
+                  handleList.SONG_LIST[i].votecount += delta;
+                  handleList.SONG_LIST[i].upbtndisable = "True";
+                } 
+              }
+              else{
+                handleList.SONG_LIST[i].votecount -= delta;
+                handleList.SONG_LIST[i].upbtndisable = "False";
+                delta = -1;
+              }
+            }
+            else {
+              if (handleList.SONG_LIST[i].downbtndisable == "False"){
+                if (handleList.SONG_LIST[i].upbtndisable == "True") {
+                    handleList.SONG_LIST[i].votecount += delta*2;
+                    delta = -2;
+                   handleList.SONG_LIST[i].downbtndisable = "True";
+                   handleList.SONG_LIST[i].upbtndisable = "False";
+                }
+                else{
+                  handleList.SONG_LIST[i].votecount += delta;
+                  handleList.SONG_LIST[i].downbtndisable = "True";
+                }
+              }
+              else{
+                handleList.SONG_LIST[i].votecount -= delta;
+                handleList.SONG_LIST[i].downbtndisable = "False";
+                delta = 1;
+              }
+            }
+
+             $.ajax({ 
+              beforeSend: function(xhr, settings) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+              },          
+              type : 'POST', 
+              url : "/api/changeVote/", 
+              data : { 
+                  'playlist' : params.playlist,
+                  'songid' : id,
+                  'delta' : delta                  
+                },
+                success: function(response) { 
+                  console.log("found playlist");
+                  temp = JSON.parse(response);
+                         
+                },
+                error: function(e, x, r) { 
+                  console.log("error - could not change vote");
+                }
+              });
+
+
+          }
+        }
+
+        clearList();
+        handleList.showList(handleList.SONG_LIST);
+        return false;
+      }
+
+      handleList.colorButtons = function() {
+        // loop through the list to see which song has been down voted and up voted, and disable that btn
+        for (var i in handleList.SONG_LIST) {
+          if (handleList.SONG_LIST[i].upbtndisable == "True") {
+            // document.getElementById('down_'+handleList.SONG_LIST[i].songid).disabled = true;
+            $('#up_'+handleList.SONG_LIST[i].songid).css("background-color", "#5da423");
+          }
+          if(handleList.SONG_LIST[i].downbtndisable == "True"){
+            // document.getElementById('up_'+handleList.SONG_LIST[i].songid).disabled = true;
+            $('#down_'+handleList.SONG_LIST[i].songid).css("background-color", "#c60f13"); 
+          } 
+        }
+      }
+
+      handleList.showList = function(list) {
+        var html = "";
+        list.sort(function(a,b) {
+          if (a.votecount > b.votecount)
+            return -1;
+          if (a.votecount < b.votecount)
+            return 1;
+          return 0;
+        });
+
+        for(var i in list) {
+          html += createPlaylistElement(list[i]);
+        }
+
+        $('#playlist').append(html);
+        handleList.colorButtons();
+        // event handler for click on upvote button
+        $('.upvotebutton').click(function(e) {
+          e.preventDefault;
+          var prefix = "up_";
+          var id = $(this).attr('id').substring(prefix.length);
+          handleList.changeVote(1,id);
+
+          voteCookie.toggleSongVoteStatus(id,1);
+          return false;
+        });
+
+        $('.downvotebutton').click(function(e) {
+          e.preventDefault;
+          var prefix = "down_";
+          var id = $(this).attr('id').substring(prefix.length);
+          handleList.changeVote(-1,id);
+
+          voteCookie.toggleSongVoteStatus(id,-1);
+          return false;
+        })
+
+      };
+  }( window.handleList = window.handleList || {}, jQuery ));
